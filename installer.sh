3e9cd4615f0498a16a4f134f49f3d40d9d6b7a16
popking159 <47087407+popking159@users.noreply.github.com>
2024-02-19 23:26:22 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # Command: wget https://raw.githubusercontent.com/popking159/ssupport/main/installer.sh -O - | /bin/sh #
-IPKFILE="subssupport_1.7.0_cortexa15hf-neon-vfpv4.ipk"
-MAINURL="https://raw.githubusercontent.com/popking159/"
+IPKFILE='subssupport_1.7.0_cortexa15hf-neon-vfpv4.ipk'
+MAINURL='https://raw.githubusercontent.com/popking159/'
 PKGDIR='ssupport/main/'
 echo "Downloading subssupport..."
 wget -T 2 $MAINURL$PKGDIR$IPKFILE -P "/tmp/"
-IPKTMPFILE="/tmp/"$IPKFILE
+IPKTMPFILE='/tmp/'$IPKFILE
 echo "Installing subssupport..."
 opkg install --force-reinstall $IPKTMPFILE
 exit 0
