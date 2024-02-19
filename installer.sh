fdc2aa771e72e251f469b3f77afe1dd43b491c86
popking159 <47087407+popking159@users.noreply.github.com>
2024-02-19 23:13:11 +0200

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+IPKFILE="subssupport_1.7.0_cortexa15hf-neon-vfpv4.ipk"
+MAINURL="https://raw.githubusercontent.com/popking159/"
+PKGDIR="ssupport/main/"
+echo "Downloading subssupport..."
+wget -T 2 $MAINURL$PKGDIR$IPKFILE -P "/tmp/"
+sleep 2
+IPKTMPFILE="/tmp/"$IPKFILE
+echo "Installing subssupport..."
+opkg install --force-reinstall $IPKTMPFILE
+sleep 2
+exit 0
