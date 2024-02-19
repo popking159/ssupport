7b337e7014087cd81f7a7310ca156931220b97d3
popking159 <47087407+popking159@users.noreply.github.com>
2024-02-19 23:14:43 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# Command: wget https://raw.githubusercontent.com/popking159/ssupport/main/installer.sh -O - | /bin/sh #
 IPKFILE="subssupport_1.7.0_cortexa15hf-neon-vfpv4.ipk"
 MAINURL="https://raw.githubusercontent.com/popking159/"
 PKGDIR="ssupport/main/"
