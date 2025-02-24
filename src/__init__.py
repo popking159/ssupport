e87c0b0c52820116348c66c5ff3d4bb440b082c9
popking159 <47087407+popking159@users.noreply.github.com>
2025-02-24 12:26:20 +0200

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -5,6 +5,8 @@ __author__ = "mx3L"
 __email__ = "mx3Lmail@gmail.com"
 __copyright__ = 'Copyright (c) 2014 mx3L'
 __license__ = "GPL-v2"
+__mod__ = "MNASR"
+__mod_email__ = "popking159@gmail.com"
 __version__ = "1.7.0_r12"
 
 
