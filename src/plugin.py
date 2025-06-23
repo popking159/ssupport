8531af4de682fc5f7739f50e8828f33790b3a7be
popking159 <47087407+popking159@users.noreply.github.com>
2025-06-23 23:18:14 +0300

    Add files via upload

--- src/plugin.py
+++ src/plugin.py
@@ -12,7 +12,7 @@ from .subtitles import E2SubsSeeker, SubsSearch, initSubsSettings, \
     SubsSetupGeneral, SubsSearchSettings, SubsSetupExternal, SubsSetupEmbedded
 from .subtitlesdvb import SubsSupportDVB, SubsSetupDVBPlayer
 
-VER = "1.7.0.19"
+VER = "1.7.0.21"
 def openSubtitlesSearch(session, **kwargs):
     settings = initSubsSettings().search
     eventList = []
