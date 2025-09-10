bb3d634acb90cce13923083e6dd8978a7f113f74
popking159 <47087407+popking159@users.noreply.github.com>
2025-09-10 23:05:40 +0300

    Add files via upload

--- src/plugin.py
+++ src/plugin.py
@@ -19,7 +19,7 @@ from .subtitles import E2SubsSeeker, SubsSearch, initGeneralSettings, initSubsSe
     SubsSetupGeneral, SubsSearchSettings, SubsSetupExternal, SubsSetupEmbedded
 from .subtitlesdvb import SubsSupportDVB, SubsSetupDVBPlayer
 
-VER = "1.7.0.28"
+VER = "1.7.0.29"
 log = logging.getLogger("SubsSupport")
 
 
