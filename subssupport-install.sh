0c90ae3612253f742fb11763da2c9528e6ee43f9
popking159 <47087407+popking159@users.noreply.github.com>
2026-06-05 23:07:10 +0300

    Update subssupport-install.sh

--- subssupport-install.sh
+++ subssupport-install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ##setup command=wget -q "--no-check-certificate" https://github.com/popking159/ssupport/raw/main/subssupport-install.sh -O - | /bin/sh
 ######### Only These two lines to edit with new version ######
-#version='1.8.0.09'
+#version='1.8.0.10'
 ##############################################################
 echo ''
 echo '************************************************************'
@@ -9,7 +9,7 @@ echo "**                       STARTED                          **"
 echo '************************************************************'
 echo "**                  Uploaded by: MNASR                    **"
 echo "************************************************************"
-echo "**                 SubsSupport v1.8 r09                   **"
+echo "**                 SubsSupport v1.8 r10                   **"
 echo "************************************************************"
 echo "************************************************************"
 sleep 3s
